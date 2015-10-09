14bfbe15eb03fab4a74685bd6809af5f56dcfa8b
Soumaya Zirari <soumaya.zirari@gmail.com>
2015-10-09 14:47:29 +0200

    Le bon commit de la semaine 1

---
+++ FormationJava/src/m2i/Formation/VenteEnLigne/Iitem.java
@@ -0,0 +1,17 @@
+package m2i.Formation.VenteEnLigne;
+
+public interface Iitem {
+
+	String getName();
+
+	void setName(String name);
+
+	int getId();
+
+	void setId(int id);
+
+	double getPrice();
+
+	void setPrice(double price);
+
+}
